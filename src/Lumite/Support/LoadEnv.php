1f92c2d3e434c48203b7e8de5db705d6a7404de7
Kashif Sohail <amkashif171@gmail.com>
2025-09-18 12:33:42 +0500

    fix dotenv issue

--- src/Lumite/Support/LoadEnv.php
+++ src/Lumite/Support/LoadEnv.php
@@ -1,13 +1,29 @@
 <?php
 
 namespace Lumite\Support;
+
 use Dotenv\Dotenv;
 
 class LoadEnv
 {
     public function __construct($path)
     {
-        $dotenv = Dotenv::createImmutable($path);
-        $dotenv->load();
+        $envFile = $path . DIRECTORY_SEPARATOR . '.env';
+
+        // Use unsafe repository so values are available via getenv() (env() helper relies on getenv)
+        $dotenv = Dotenv::createUnsafeImmutable($path);
+
+        if (file_exists($envFile)) {
+            $dotenv->load();
+        } else {
+            if (!defined('ENV_FILE_MISSING')) {
+                define('ENV_FILE_MISSING', true);
+            }
+            if (method_exists($dotenv, 'safeLoad')) {
+                $dotenv->safeLoad();
+            } else {
+                try { $dotenv->load(); } catch (\Throwable $e) { /* ignore when missing */ }
+            }
+        }
     }
 }
